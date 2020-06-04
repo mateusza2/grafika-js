35c40dc532319bc0a10690cd285eb12159ff9eb9
Mateusz Adamowski <mateusz@adamowski.pl>
2020-06-04 18:25:52 +0200

    rysowanie wielu linii i łuków

--- rysowanie.js
+++ rysowanie.js
@@ -1,5 +1,30 @@
 function rysuj(){
-    console.log("Rysuj!!!")
+    var cnv = document.querySelector("#c1")
+    var ctx = cnv.getContext('2d')
 
+    ctx.beginPath();
+    ctx.lineWidth = 10
+    ctx.strokeStyle = "#e22"
+    ctx.moveTo( 350, 200 )
+    ctx.lineTo( 700, 550 )
+    ctx.stroke()
+
+
+    ctx.beginPath();
+    ctx.lineWidth = 5
+    ctx.strokeStyle = "#33e"
+    ctx.moveTo( 600, 400 )
+    ctx.lineTo( 620, 350 )
+    ctx.moveTo( 500, 120 )
+    ctx.lineTo( 420, 90 )
+    ctx.moveTo( 300, 80 )
+    ctx.lineTo( 200, 100 )
+    ctx.stroke()
+    
+    ctx.beginPath();
+    ctx.lineWidth = 12
+    ctx.strokeStyle = "#ee2"
+    ctx.arc( 400, 300, 200, 0, 2 * Math.PI )
+    ctx.stroke()
 
 }
